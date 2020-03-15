d7be46af77c5ab111601028ac060a9905a7588cf
genneko <genneko@users.noreply.github.com>
2020-03-16 08:51:28 +0900

    Import Mk/Uses/*.mk from tagattie/FreeBSD-Electron

--- _utils/prepare-for-electron-ports.sh
+++ _utils/prepare-for-electron-ports.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # prepare-for-electron-ports.sh
 #
+prog=$(basename $0)
+bindir=$(dirname $(readlink -f $0))
+basedir=$(readlink -f $bindir/..)
 portsdir=${PORTSDIR:-/usr/ports}
-electron_portdir=${ELECTRON_PORTDIR:-$HOME/src/FreeBSD-Electron}
 sudo=""
 
 perr() {
@@ -22,28 +24,20 @@ else
 	exit 1
 fi
 
-if [ -d "$electron_portdir" ]; then
-	perr "[INFO] Okay. $electron_portdir exists."
-else
-	perr "[CRITICAL] $electron_portdir doesn't exist. Get it now."
-	perr "git clone https://github.com/tagattie/FreeBSD-Electron.git"
-	exit 1
-fi
-
 if [ -d "$portsdir/devel/electron7" ]; then
 	perr "[INFO] $portsdir/devel/electron7 already exists."
 else
-	$sudo ln -s "$electron_portdir/devel/electron7" $portsdir/devel \
-		&& perr "[NOTICE] Created a link $portsdir/devel/electron7" \
-		|| perr "[ERROR] Failed to created $portsdir/devel/electron7" 
+	perr "[CRITICAL] $portsdir/devel/electron7 doesn't exist. Get it now."
+	perr "portsnap fetch update"
+	exit 1
 fi
 
-cd "$electron_portdir/Mk/Uses"
+cd "$basedir/Mk/Uses"
 for mk in *.mk; do
 	if [ -f "$portsdir/Mk/Uses/$mk" ]; then
 		perr "[INFO] $portsdir/Mk/Uses/$mk already exists."
 	else
-		$sudo ln -s "$electron_portdir/Mk/Uses/$mk" $portsdir/Mk/Uses \
+		$sudo ln -s "$basedir/Mk/Uses/$mk" $portsdir/Mk/Uses \
 		&& perr "[NOTICE] Created a link $portsdir/Mk/Uses/$mk" \
 		|| perr "[ERROR] Failed to created $portsdir/Mk/Uses/$mk"
 	fi
